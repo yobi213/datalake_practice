18701f6a4626ca6d9b6b8bf1653dbe348fb42999
yobi213 <39682914+yobi213@users.noreply.github.com>
2020-05-02 18:46:57 +0900

    Update twitter_config.py

--- twitter_config.py
+++ twitter_config.py
@@ -1,14 +1,4 @@
-#These information comes from Twitter API
-#Create a Twitter Account and Get These Information from apps.twitter.com
-#consumer_key = "fPfZwgsRCVXCflAzZ9WQLIN7K"
-#consumer_secret = "Y9pDFu4g1OecMpjNhue70kMvscL4NgEp4HyEAPN8WSmCN1aCLQ"
-#access_token = "616123627-gAkuVe7rHolE76BozTWteHByh760DNpjO6OplOwp"
-#access_secret = "oSDservoNxKaPSvLP1wHOhp6WrNgK9yavXnWxF8CfwM1D"
-
-
-
-
-consumer_key = "A7XlhgAUCYa4TAVNkGZCNFrFa"
-consumer_secret = "zKDTmUV3mrgy96CcIeGyDqxA4e7LniNgbIFFXMsZM9XGyxzKhe"
-access_token = "22082713-EkSh3lYwV4Gs0Lc1luQUoQ1IBSrQxSIxCn7gyfyqE"
-access_secret = "14rx7wDxoprk4cvTIOX9KrI0advwpZ3IjjmWZYnFLbaQf"
+consumer_key = "***"
+consumer_secret = "***"
+access_token = "***"
+access_secret = "***"
